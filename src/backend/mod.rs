d8924f7262df118dba16e7fbfb5d3d3006a0fcb1
Xor290 <free6nk33@gmail.com>
2026-04-12 17:06:36 +0200

    chore: add asyncrone support for backend vpn-core lib

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,9 +1,11 @@
+pub mod async_core;
 pub mod core;
-
 #[cfg(feature = "http-backend")]
 pub mod http;
 
-pub use core::{AuthResponse, BackendError, ConnectionInfo, PeerStatus, Server, UserInfo, VpnBackend};
+pub use core::{
+    AuthResponse, BackendError, ConnectionInfo, PeerStatus, Server, UserInfo, VpnBackend,
+};
 
 #[cfg(feature = "http-backend")]
 pub use http::HttpBackend;
