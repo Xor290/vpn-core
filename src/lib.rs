a099da347f82d26bf6ca3f1c8704035f894380bf
Xor290 <free6nk33@gmail.com>
2026-04-12 09:11:22 +0200

    chore: update lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@ pub mod backend;
 pub mod session;
 pub mod wireguard;
 
-/// Alias de commodité pour une session utilisant le backend HTTP par défaut.
 #[cfg(feature = "http-backend")]
 pub type HttpSession = session::Session<backend::HttpBackend>;
